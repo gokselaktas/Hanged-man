ed76feae6d83cb19d1a50b260c0518099aabad74
gokselaktas <aktas_goksel@hotmail.com>
2019-04-20 22:53:29 +0300

    Main

--- game.py
+++ game.py
@@ -1,167 +1,45 @@
 import time
 import random
 import os
+from hangedman import *
+from functions import *
+from choose_word import choose_word
 
-print("---HANGMAN---\n\n")
-time.sleep(0.5)
+def game():
+    guesses = ''
+    turns = 10
+    while turns > 0:
+        failed = 0
+        for char in word:
+            if char in guesses:
+                print (char, end = " ")
+            else:
+                print("_", end=" ")
+                failed += 1
 
-def clear_screen():#ekrani temizlemek icin
-    os.system('clear')
-    return
+        if failed == 0:
+            print("\n Kazandin")
+            break
 
-alphabet=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-def print_alphabet(alphabet):
-    x = '|'.join(alphabet)
-    print("Kullanilabilir harfler=> ",x)
-
-
-triedword=[]
-def inputal():#kullanicindan input alma sartlari
-    try:
-        while True:
-            time.sleep(0.3)
-            harf = input("Harf giriniz\n")
-            if harf == word:
-                break
-            elif len(harf)>1 :
-                print("harf girmeniz lazim\n")
-            elif harf in triedword:
-                print("Kullandiginiz harfi tekrar kullandiniz")
-            elif len(harf)==1:
-                triedword.append(harf)
-                alphabet.remove(harf)
-                break
-    except ValueError:
-        print("harf veya tahmin girmediniz.")
-        triedword.remove(harf)
-        time.sleep(1)
-    return harf
-
-#text dosyasindan kelime okuma
-f=open("wordlist.txt","r")
-content = f.read()
-wordlist = content.split("\n")
-i = random.randint(0,len(wordlist)-1)
-word = wordlist[i]
-################################################################################
-
-
-
-def copadam(turns):
-    copadam=[
-            "           \n"
-            "           \n"
-            "           \n"
-            "           \n"
-            "           \n"
-            "           \n",
-
-            "            \n"
-            "  |         \n"
-            "  |         \n"
-            "  |         \n"
-            "  |         \n"
-            " _|         \n",
-
-            "  _______   \n"
-            "  |         \n"
-            "  |         \n"
-            "  |         \n"
-            "  |         \n"
-            " _|         \n",
-
-            "  _______   \n"
-            "  | /       \n"
-            "  |/        \n"
-            "  |         \n"
-            "  |         \n"
-            " _|         \n",
-
-            "  _______   \n"
-            "  | /   '   \n"
-            "  |/        \n"
-            "  |         \n"
-            "  |         \n"
-            " _|         \n",
-
-            "  _______   \n"
-            "  | /   '   \n"
-            "  |/    O   \n"
-            "  |         \n"
-            "  |         \n"
-            " _|         \n",
-
-            "  _______   \n"
-            "  | /   '   \n"
-            "  |/    O   \n"
-            "  |     |   \n"
-            "  |         \n"
-            " _|         \n",
-
-
-            "  _______   \n"
-            "  | /   '   \n"
-            "  |/    O   \n"
-            "  |     |   \n"
-            "  |    /    \n"
-            " _|         \n",
-
-            "  _______   \n"
-            "  | /   '   \n"
-            "  |/    O   \n"
-            "  |     |   \n"
-            "  |    / \\ \n"
-            " _|         \n",
-
-            "  _______   \n"
-            "  | /   '   \n"
-            "  |/   \\O  \n"
-            "  |     |   \n"
-            "  |    / \\ \n"
-            " _|         \n",
-
-            "  _______   \n"
-            "  | /   '   \n"
-            "  |/   \\O/ \n"
-            "  |     |   \n"
-            "  |    / \\ \n"
-            " _|         \n",
-
-            ]
-    print(turns,"hakkiniz kaldi.")
-    print(copadam[10-turns])
-
-
-guesses = ''
-turns = 10
-while turns>0:
-    failed=0
-    for char in word:
-        if char in guesses:
-            print (char,end=" ")
+        guess,turns = inputal(word,turns)
+        guesses += guess
+        if guess not in word:
+            turns -= 1
+            clear_screen()
+            print_alphabet(alphabet)
+            print("\nYanlış Bildiniz")
+            copadam(turns)
+            if turns == 0:
+                print("Kelime => ", word," <=")
+                print("\nKaybettiniz")
         else:
-            print("_",end=" ")
-            failed+=1
+            clear_screen()
+            print_alphabet(alphabet)
+            copadam(turns)
 
-    if failed==0:
-        print("\n Kazandin")
-        break
 
-    print("\n")
-    guess = inputal()
-    guesses += guess
-    if guess not in word:
-        turns -=1
-        clear_screen()
-        print_alphabet(alphabet)
-        print("\nYanlış Bildiniz")
-        copadam(turns)
-        if turns == 0:
-            print("Kelime =>",word)
-            print("\nKaybettiniz")
-    else:
-        clear_screen()
-        print_alphabet(alphabet)
-        copadam(turns)
+
+gamestart()
+time.sleep(0.321)
+word = choose_word()
+game()
